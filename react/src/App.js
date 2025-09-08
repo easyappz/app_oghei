2697d07a96671e92ff2a8a957bd15aad6bde81f0
IbragimKhaniev <newinterny@gmail.com>
2025-09-09 01:24:43 +0300

    Update files

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
 import './App.css';
 import PaymentsDashboard from './pages/PaymentsDashboard';
@@ -7,12 +7,10 @@ import PaymentsDashboard from './pages/PaymentsDashboard';
 function App() {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<PaymentsDashboard />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaymentsDashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </ErrorBoundary>
   );
 }
